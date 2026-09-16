058253dd08a15fa6ed4a0ad86fd03be770e56a0d
pawelmalecki <pawelekm045@gmail.com>
2026-09-16 23:50:20 +0200

    #Wyświetl liczby całkowite od 1 do 100. Spróbuj zrobić to, wykorzystując różne #pętle (for, while, do..while). [1]

---
+++ zadanie_1.10.py
@@ -0,0 +1,8 @@
+#Wyświetl liczby całkowite od 1 do 100. Spróbuj zrobić to, wykorzystując różne
+#pętle (for, while, do..while). [1]
+for i in range(1, 101):
+    print(i)
+j :int = 1
+while(j <= 100):
+    print(j)
+    j += 1
