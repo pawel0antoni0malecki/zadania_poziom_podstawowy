2b42d16ae34c16b6bbd3bcfa8ff2e928df622cbf
pawelmalecki <pawelekm045@gmail.com>
2026-09-16 23:21:19 +0200

    #Pobieraj znaki z klawiatury aż do wprowadzenia znaku x. Ile znaków pobrano? [1]

---
+++ zadanie_1.7.py
@@ -0,0 +1,9 @@
+#Pobieraj znaki z klawiatury aż do wprowadzenia znaku x. Ile znaków pobrano? [1]
+ile_podano :int = 0
+while(True):
+    if "x" == input("podaj znak : "):
+        ile_podano += 1
+        break
+    else:
+        ile_podano += 1
+print("podano "+str(ile_podano)+" znaków")
