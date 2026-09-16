b921e43d970c323450518336a906017d689fda51
pawelmalecki <pawelekm045@gmail.com>
2026-09-16 21:26:39 +0200

    #Masz takie wyrażenie: (((a1+a2)*a3)-a4)/a5 (elementy od a1 do a5 są typu float). #Pobierz z klawiatury każdą ze zmiennych a1 do a5, oblicz wartość wyrażenia #i wyświetl wynik. [1]

---
+++ zadanie_1.5.py
@@ -0,0 +1,7 @@
+#Masz takie wyrażenie: (((a1+a2)*a3)-a4)/a5 (elementy od a1 do a5 są typu float).
+#Pobierz z klawiatury każdą ze zmiennych a1 do a5, oblicz wartość wyrażenia
+#i wyświetl wynik. [1]
+numer : list[float] = []
+for i in range(0, 5):
+    numer.append( float( input("podaj liczbę numer " + str(i+1)+": ")))
+print("wynik : "+ str( (((numer[0]+numer[1]) * numer[2]) - numer[3]) / numer[4]))
