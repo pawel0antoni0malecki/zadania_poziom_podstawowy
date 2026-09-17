0feaddddb5e666fda612595d88514633598befde
pawelmalecki <pawelekm045@gmail.com>
2026-09-17 03:42:17 +0200

    #Pobieraj liczby z klawiatury i wkładaj je do tablicy/wektora. Pobieranie ma się #zakończyć, gdy zostanie wprowadzona dwa razy z rzędu taka sama liczba. [1]

---
+++ zadanie_1.22.py
@@ -0,0 +1,8 @@
+#Pobieraj liczby z klawiatury i wkładaj je do tablicy/wektora. Pobieranie ma się
+#zakończyć, gdy zostanie wprowadzona dwa razy z rzędu taka sama liczba. [1]
+zbiur :list[int] = []
+zbiur.append(int(input("podaj liczbę : ")))
+while(True):
+    zbiur.append(int(input("podaj liczbę : ")))
+    if zbiur[len(zbiur)-2] == zbiur[len(zbiur)-1]:
+        break
