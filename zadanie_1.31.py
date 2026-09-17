3b127fefce29d359899be81160ebcec825cce67d
pawelmalecki <pawelekm045@gmail.com>
2026-09-17 15:34:58 +0200

    Spróbuj odpowiedzieć na pytania do podanego kodu, wykorzystując tylko swoją wiedzę i nie używając kompilatora: int a = 0; // a) Co się wyświetli w dwóch następnych instrukcjach? Wyjaśnij to. cout << ++a << endl; cout << a << endl; // b) Co się wyświetli po wykonaniu dwóch poniższych instrukcji? Wyjaśnij to. cout << (a++ - 1 && 1) << endl; cout << a << endl; // c) A co tu się wyświetli? cout << 1 + 2 * 3 - 4 / 5 << endl; // d) Poniższa instrukcja nie jest poprawna. Potrafisz wyjaśnić dlaczego? cout << true and false << endl; Pytania znajdują się w komentarzach i zostały oznaczone jako a), b), c) i d). [1]

---
+++ zadanie_1.31.py
@@ -0,0 +1,14 @@
+#Spróbuj odpowiedzieć na pytania do podanego kodu, wykorzystując tylko swoją
+#wiedzę i nie używając kompilatora:
+#int a = 0;
+#// a) Co się wyświetli w dwóch następnych instrukcjach? Wyjaśnij to.
+#cout << ++a << endl;
+#cout << a << endl;
+#// b) Co się wyświetli po wykonaniu dwóch poniższych instrukcji? Wyjaśnij to.
+#cout << (a++ - 1 && 1) << endl;
+#cout << a << endl;
+#// c) A co tu się wyświetli?
+#cout << 1 + 2 * 3 - 4 / 5 << endl;
+#// d) Poniższa instrukcja nie jest poprawna. Potrafisz wyjaśnić dlaczego?
+#cout << true and false << endl;
+#Pytania znajdują się w komentarzach i zostały oznaczone jako a), b), c) i d). [1]
