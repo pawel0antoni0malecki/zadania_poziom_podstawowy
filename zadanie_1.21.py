1a30292f91da101a85ed7504b50c081209f0b0b0
pawelmalecki <pawelekm045@gmail.com>
2026-09-17 03:34:07 +0200

    #Pobierz pięć liczb z klawiatury. Jeżeli tworzą ciąg rosnący (zgodnie z kolejnością #pobierania), poinformuj o tym. [1]

---
+++ zadanie_1.21.py
@@ -0,0 +1,16 @@
+#Pobierz pięć liczb z klawiatury. Jeżeli tworzą ciąg rosnący (zgodnie z kolejnością
+#pobierania), poinformuj o tym. [1]
+zbiur :list[int] = []
+zbiur.append(int(input("Podaj liczbę nr 1 : ")))
+zbiur.append(int(input("Podaj liczbę nr 2 : ")))
+zbiur.append(int(input("Podaj liczbę nr 3 : ")))
+zbiur.append(int(input("Podaj liczbę nr 4 : ")))
+zbiur.append(int(input("Podaj liczbę nr 5 : ")))
+test :bool = True
+for i in range(0, 4):
+    if zbiur[i] > zbiur[i+1]:
+        test = False
+if test:
+    print("zbiur jest rosnący")
+else:
+    print("zbiur nie jest rosnący")
