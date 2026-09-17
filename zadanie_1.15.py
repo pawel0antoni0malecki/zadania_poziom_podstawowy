f8950c0fada36d620a754d87031d25ccd9493541
pawelmalecki <pawelekm045@gmail.com>
2026-09-17 02:25:06 +0200

    Odgadnij wzór ciągu, a następnie wyświetl jego sto elementów. Początek ciągu: 1,2,2,3,3,3,4,4,4,4,.... [2]

---
+++ zadanie_1.15.py
@@ -0,0 +1,10 @@
+#Odgadnij wzór ciągu, a następnie wyświetl jego sto elementów. Początek ciągu:
+#1,2,2,3,3,3,4,4,4,4,.... [2]'=
+startowa :int = 1
+powturki :int = 0
+for i in range(1, 101):
+    print(startowa)
+    powturki += 1
+    if startowa == powturki:
+        startowa += 1
+        powturki = 0
