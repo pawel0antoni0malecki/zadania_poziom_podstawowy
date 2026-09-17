3e26eb9ff0807925448942f05b95aaf709960441
pawelmalecki <pawelekm045@gmail.com>
2026-09-17 02:58:05 +0200

    #Utwórz wektor i wstaw do niego dziesięć dowolnych liczb. Utwórz drugi wektor, #który na początku zawiera liczby parzyste z pierwszego wektora, a potem po- #zostałe. Wyświetl oba wektory. [1]

---
+++ zadanie_1.20.py
@@ -0,0 +1,20 @@
+#Utwórz wektor i wstaw do niego dziesięć dowolnych liczb. Utwórz drugi wektor,
+#który na początku zawiera liczby parzyste z pierwszego wektora, a potem po-
+#zostałe. Wyświetl oba wektory. [1]
+import random
+
+wektor :list[int] = []
+wektor_2 :list[int] = []
+
+for i in range(0, 10):
+    wektor.append(random.randint(1, 100))
+
+for i in range(0, len(wektor)):
+    if 0 == wektor[i] % 2:
+        wektor_2.append(wektor[i])
+
+for i in range(0, len(wektor)):
+    if 0 != wektor[i] % 2:
+        wektor_2.append(wektor[i])
+print(wektor)
+print(wektor_2)
