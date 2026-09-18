7e48e2a62f83f7d0b9c2854e86c8921ef6fae9f7
pawelmalecki <pawelekm045@gmail.com>
2026-09-18 19:25:36 +0200

    #Wylosuj i wyświetl liczbę ułamkową (typ double) w zakresie 0 do 1 włącznie #z maksymalnie trzema miejscami po przecinku. Skorzystaj z funkcji rand(). [1]

---
+++ Rozdział 2/zadanie_2.8.py
@@ -0,0 +1,5 @@
+#Wylosuj i wyświetl liczbę ułamkową (typ double) w zakresie 0 do 1 włącznie
+#z maksymalnie trzema miejscami po przecinku. Skorzystaj z funkcji rand(). [1]
+import random
+liczba :float = round(random.random(), 3)
+print(liczba)
